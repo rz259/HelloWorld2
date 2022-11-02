72d187a96852214c8d7de5969ac460b81276c174
Rudi <rudolf.ziegaus@gmx.de>
2022-11-02 14:48:51 +0100

    renamed test class to HelloWorld2Test

---
+++ src/test/java/de/rziegaus/test/HelloWorld2Test.java
@@ -0,0 +1,30 @@
+package de.rziegaus.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import de.rziegaus.HelloWorld2;
+
+class HelloWorld2Test
+{
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception
+	{
+	}
+
+	@BeforeEach
+	void setUp() throws Exception
+	{
+	}
+
+	@Test
+	void testGetgGreeting()
+	{
+		assertEquals(HelloWorld2.getGreeting(), "Hello World 2");
+	}
+
+}
